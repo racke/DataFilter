5a3a307c24c7aa1988fc04a20d55ea79efdcef74
racke <racke>
2004-07-27 11:32:04 +0000

    simple converter module

---
+++ DataFilter/Converter.pm
@@ -0,0 +1,57 @@
+#! /usr/bin/perl
+#
+# Copyright 2004 by Stefan Hornburg (Racke) <racke@linuxia.de>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public
+# License along with this program; if not, write to the Free
+# Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+# MA  02111-1307  USA.
+
+package DataFilter::Converter;
+use strict;
+
+sub new {
+	my $proto = shift;
+	my $class = ref ($proto) || $proto;
+	my $self = {@_};
+	
+	bless ($self, $class);
+	$self->{_defines_} = {};
+	return $self;
+}
+
+sub define {
+	my ($self, $source, $target) = @_;
+
+	$self->{_defines_}->{$source} = $target;
+}
+
+sub convert {
+	my ($self, $record) = @_;
+	my %new_record;
+	
+	if ($self->{DEFINED_ONLY}) {
+		for (keys %{$self->{_defines_}}) {
+			$new_record{$self->{_defines_}->{$_}} = $record->{$_};
+		}
+	} else {
+		%new_record = %$record;
+		for (keys %{$self->{_defines_}}) {
+			$new_record{$self->{_defines_}->{$_}} = delete $new_record{$_};
+		}
+	}
+
+	return \%new_record;
+}
+
+1;
