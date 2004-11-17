9271686ce7e72b32c37db62fd8e448d1c95cda37
racke <racke>
2004-11-17 12:14:02 +0000

    support code for Interchange

---
+++ interchange/Vend/DataFilter.pm
@@ -0,0 +1,41 @@
+# Vend::DataFilter - Interchange image helper functions
+#
+# Copyright (C) 2004 Stefan Hornburg (Racke) <racke@linuxia.de>.
+
+package Vend::DataFilter;
+
+use strict;
+use warnings;
+
+use DataFilter;
+
+sub datafilter {
+	my ($opt) = @_;
+	my ($tmpfile);
+
+	my $source = $opt->{source};
+	
+	my ($df, $df_source);
+	
+	$df = new DataFilter;
+
+	if ($source->{type} eq 'XLS') {
+		# we need to store the input as temporary file first
+		$tmpfile = "tmp/df-$Vend::Session->{id}-$Vend::Session->{pageCount}.xls";
+		Vend::Tags->write_relative_file($tmpfile, \$CGI::file{$source->{name}});
+
+		$df_source = $df->source(type => $source->{type},
+								 name => $tmpfile,
+								 verify => 1);
+	}
+	
+	unless ($df_source) {
+		return $df->error();
+	}
+
+	if ($opt->{return} eq 'columns') {
+		return join(',', $df_source->columns());
+	}
+}
+
+1;
---
+++ interchange/usertag/datafilter.tag
@@ -0,0 +1,4 @@
+Require module Vend::DataFilter
+
+UserTag datafilter AddAttr
+UserTag datafilter MapRoutine Vend::DataFilter::datafilter
