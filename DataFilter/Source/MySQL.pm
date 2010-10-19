b599eaccfdf8234cc27ae8f7fe9e4d553b437c77
Stefan Hornburg (Racke) <racke@linuxia.de>
2010-10-19 20:31:44 +0200

    Update copyright dates.

--- DataFilter/Source/MySQL.pm
+++ DataFilter/Source/MySQL.pm
@@ -1,6 +1,6 @@
 #! /usr/bin/perl
 #
-# Copyright 2004 by Stefan Hornburg (Racke) <racke@linuxia.de>
+# Copyright 2004-2010 by Stefan Hornburg (Racke) <racke@linuxia.de>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
--- DataFilter/Source/SQL.pm
+++ DataFilter/Source/SQL.pm
@@ -1,7 +1,7 @@
 #
 # SQL.pm - base class for RDBMS
 #
-# Copyright 2007 by Stefan Hornburg (Racke) <racke@linuxia.de>
+# Copyright 2007-2010 by Stefan Hornburg (Racke) <racke@linuxia.de>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
